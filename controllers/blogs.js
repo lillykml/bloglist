9af8947fdac5fd7fb829752888a7a7e37cf58ce0
lillykml <64362067+lillykml@users.noreply.github.com>
2024-06-04 15:29:06 +0200

    Tested & re-factored post-request

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -15,13 +15,10 @@ blogRouter.get('/:id', (request, response, next) => {
   .catch(error => next(error))
 })
   
-blogRouter.post('/', (request, response) => {
+blogRouter.post('/', async (request, response) => {
     const blog = new Blog(request.body)
-    blog
-        .save()
-        .then(result => {
-        response.status(201).json(result)
-        })
+    const result = await blog.save()
+    response.status(201).json(result)
 })
 
 module.exports = blogRouter
