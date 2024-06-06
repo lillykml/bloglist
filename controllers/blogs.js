cb398e1230a99b3501f0971e1814afc8c410cd37
lillykml <64362067+lillykml@users.noreply.github.com>
2024-06-06 10:01:10 +0200

    Fixed get users & get blogs

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,8 +1,9 @@
 const blogRouter = require('express').Router()
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
 blogRouter.get('/', async (request, response) => {
-  const blogs = await Blog.find({})
+  const blogs = await Blog.find({}).populate('user', {username: 1, name: 1})
   response.json(blogs)
 })
 
@@ -12,8 +13,18 @@ blogRouter.get('/:id', async (request, response) => {
 })
   
 blogRouter.post('/', async (request, response) => {
-    const blog = new Blog(request.body)
+
+    const user = await User.findOne({})
+    const blogData = {
+      ...request.body,
+      user: user.id
+    };
+
+    const blog = new Blog(blogData)
     const result = await blog.save()
+
+    user.blogs = user.blogs.concat(blog._id)
+    await user.save()
     response.status(201).json(result)
 })
 
